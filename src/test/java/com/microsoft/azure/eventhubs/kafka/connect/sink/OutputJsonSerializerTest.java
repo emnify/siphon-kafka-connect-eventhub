f902c66ae4c8d8467c55c2b1adda5c4f966538ff
Denys Kovalevskyi <denys.kovalevskyi@emnify.com>
2025-05-12 16:32:18 +0200

    Update dependencies

--- src/test/java/com/microsoft/azure/eventhubs/kafka/connect/sink/OutputJsonSerializerTest.java
+++ src/test/java/com/microsoft/azure/eventhubs/kafka/connect/sink/OutputJsonSerializerTest.java
@@ -3,7 +3,7 @@ package com.microsoft.azure.eventhubs.kafka.connect.sink;
 import org.apache.kafka.connect.data.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.Instant;
 
